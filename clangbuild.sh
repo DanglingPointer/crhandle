4d7ec47412d4a54afc60b5f9bf9361d31f0df33f
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2022-06-21 17:19:30 +0200

    Add script for building with clang on Linux

---
+++ clangbuild.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+mkdir -p cmake-build-manual
+cd cmake-build-manual || exit
+if test "$1" = "--clean"; then
+    rm -rf *;
+fi
+
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -Dcrhandle_build_tests=ON \
+  -DCMAKE_CXX_COMPILER=clang++-12
+cmake --build . && ctest -VV
