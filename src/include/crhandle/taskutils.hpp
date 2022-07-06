4c452be87b611fd5ecbf8fab15d67ae22a32a31e
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2022-07-06 17:14:42 +0200

    AnyOf: simplify NonVoid

--- src/include/crhandle/taskutils.hpp
+++ src/include/crhandle/taskutils.hpp
@@ -6,6 +6,7 @@
 #include <array>
 #include <optional>
 #include <tuple>
+#include <variant>
 
 namespace cr {
 
@@ -33,24 +34,15 @@ auto CreateArray(std::index_sequence<Is...>, T && tuple, F && transform)
 }
 
 template <typename T>
-struct TypeConverter
+struct NonVoid
 {
    using Type = T;
 };
 
 template <>
-struct TypeConverter<void>
+struct NonVoid<void>
 {
    using Type = std::monostate;
-};
-
-template <typename T>
-struct NonVoid;
-
-template <template <typename...> typename C, typename... Ts>
-struct NonVoid<C<Ts...>>
-{
-   using Type = C<typename TypeConverter<Ts>::Type...>;
 };
 
 } // namespace internal
@@ -61,12 +53,12 @@ using NonVoid = typename internal::NonVoid<T>::Type;
 struct AnyOfFn
 {
    template <Executor E, TaskResult... Rs>
-   using HandleType = TaskHandle<NonVoid<std::variant<Rs...>>, E>;
+   using HandleType = TaskHandle<std::variant<NonVoid<Rs>...>, E>;
 
    template <Executor E, TaskResult... Rs>
    HandleType<E, Rs...> operator()(TaskHandle<Rs, E>... ts) const
    {
-      std::optional<NonVoid<std::variant<Rs...>>> ret;
+      std::optional<std::variant<NonVoid<Rs>...>> ret;
       stdcr::coroutine_handle<> continuation = nullptr;
 
       auto TaskWrapper = [&]<size_t I, typename R>(std::in_place_index_t<I> i,
@@ -76,7 +68,7 @@ struct AnyOfFn
 
          if constexpr (std::is_same_v<R, void>) {
             co_await std::move(task);
-            ret.emplace(i, internal::TypeConverter<void>::Type{});
+            ret.emplace(i, NonVoid<void>{});
          } else {
             R tmp = co_await std::move(task);
             ret.emplace(i, std::move(tmp));
